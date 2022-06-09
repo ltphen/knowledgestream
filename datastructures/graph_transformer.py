300efc9fbfa078f7a7ce4ef8df95af49363a4e8e
Sascha Trippel <strippel@mail.uni-paderborn.de>
2022-06-09 15:25:11 +0200

    Implemented ID generation

--- datastructures/graph_transformer.py
+++ datastructures/graph_transformer.py
@@ -7,7 +7,10 @@ class GraphTransformer:
     """
 
     def __init__(self):
-        self.id = dict()
+        self.nodeId = dict()
+        self.relId = dict()
+        self.nodeIdCount = 0
+        self.relIdCount = 0
 
     def generateAdjacency(self, graphPath):
         rdfGraph = self._readTurtleGraph(graphPath)
@@ -15,7 +18,34 @@ class GraphTransformer:
 
 
     def _generateIndices(self, rdfGraph):
-        pass
+        for sub, pred, obj in rdfGraph:
+            # Set subject id
+            try:
+                self.nodeId[sub]
+            except KeyError:
+                self.nodeId[sub] = self._nextNodeId()
+
+            # Set object id
+            try:
+                self.nodeId[obj]
+            except KeyError:
+                self.nodeId[obj] = self._nextNodeId()
+
+            # Set predicate id
+            try:
+                self.relId[pred]
+            except KeyError:
+                self.relId[pred] = self._nextRelationshipId()
+
+    def _nextNodeId(self):
+        nextId = self.nodeIdCount
+        self.nodeIdCount += 1
+        return nextId
+
+    def _nextRelationshipId(self):
+        nextId = self.relIdCount
+        self.relIdCount += 1
+        return nextId
 
     def _readTurtleGraph(self, graphPath):
         rdfGraph = RdfGraph()
