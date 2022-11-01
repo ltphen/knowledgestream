de4148e4384204379dae0974be76bcd823ffc1a9
Sascha Trippel <strippel@mail.uni-paderborn.de>
2022-11-01 11:19:53 +0100

    Cleaned main

--- algorithms/__main__.py
+++ algorithms/__main__.py
@@ -11,15 +11,12 @@ import socket
 import numpy as np
 import pandas as pd
 import warnings
-import ujson as json
 import logging as log
 
 from pandas import DataFrame, Series
 from os.path import expanduser, abspath, isfile, isdir, basename, splitext, \
 	dirname, join, exists
 from time import time
-from datetime import date
-import cPickle as pkl
 
 from datastructures.rgraph import Graph, weighted_degree
 from rdflib import Graph as RDFGraph
@@ -65,9 +62,6 @@ WTFN = 'logdegree'
 RELSIMPATH = join(HOME, 'relsim/predicate-similarity.npy') 
 # assert exists(RELSIMPATH)
 
-# Date
-DATE = '{}'.format(date.today())
-
 # data types for int and float
 _short = np.int16
 _int = np.int32
@@ -102,7 +96,7 @@ measure_map = {
 	}
 }
 
-# prefix dict
+# prefix dict TODO: remove
 prefix = dict()
 prefix['dbo'] = "http://dbpedia.org/ontology/"
 prefix['dbp'] = "http://dbpedia.org/property/"
@@ -177,76 +171,6 @@ def compute_mincostflow(G, relsim, subs, preds, objs):
         np.copyto(G.csr.indices, G_bak['indices'])
         np.copyto(G.csr.indptr, G_bak['indptr'])
         np.copyto(cost_vec, cost_vec_bak)
-	return mincostflows, times
-
-def compute_mincostflow_legacy(G, relsim, subs, preds, objs, flowfile):
-	"""
-	Parameters:
-	-----------
-	G: rgraph
-		See `datastructures`.
-	relsim: ndarray
-		A square matrix containing relational similarity scores.
-	subs, preds, objs: sequence
-		Sequences representing the subject, predicate and object of 
-		input triples.
-	flowfile: str
-		Absolute path of the file where flow will be stored as JSON,
-		one line per triple.
-
-	Returns:
-	--------
-	mincostflows: sequence
-		A sequence containing total flow for each triple.
-	times: sequence
-		Times taken to compute stream of each triple. 
-	"""
-	# take graph backup
-	G_bak = {
-		'data': G.csr.data.copy(), 
-		'indices': G.csr.indices.copy(),
-		'indptr': G.csr.indptr.copy()
-	}
-	cost_vec_bak = np.log(G.indeg_vec).copy()
-
-	# some set up
-	G.sources = np.repeat(np.arange(G.N), np.diff(G.csr.indptr))
-	G.targets = G.csr.indices % G.N
-	cost_vec = cost_vec_bak.copy()
-	indegsim = weighted_degree(G.indeg_vec, weight=WTFN)
-	specificity_wt = indegsim[G.targets] # specificity
-	relations = (G.csr.indices - G.targets) / G.N
-	mincostflows, times = [], []
-	with open(flowfile, 'w', 0) as ff:
-		for idx, (s, p, o) in enumerate(zip(subs, preds, objs)):
-			s, p, o = [int(x) for x in (s, p, o)]
-			ts = time()
-                        if len(subs) > 1:
-			    print '{}. Working on {} .. '.format(idx+1, (s, p, o)),
-			    sys.stdout.flush()
-
-			# set weights
-			relsimvec = np.array(relsim[p, :]) # specific to predicate p
-			relsim_wt = relsimvec[relations]
-			G.csr.data = np.multiply(relsim_wt, specificity_wt)
-			
-			# compute
-			mcflow = succ_shortest_path(
-				G, cost_vec, s, p, o, return_flow=False, npaths=5
-			)
-			mincostflows.append(mcflow.flow)
-			ff.write(json.dumps(mcflow.stream) + '\n')
-			tend = time()
-			times.append(tend - ts)
-                        if len(subs) > 1:
-			    print 'mincostflow: {:.5f}, #paths: {}, time: {:.2f}s.'.format(
-				mcflow.flow, len(mcflow.stream['paths']), tend - ts)
-
-			# reset state of the graph
-			np.copyto(G.csr.data, G_bak['data'])
-			np.copyto(G.csr.indices, G_bak['indices'])
-			np.copyto(G.csr.indptr, G_bak['indptr'])
-			np.copyto(cost_vec, cost_vec_bak)
 	return mincostflows, times
 
 # ================= RELATIONAL KNOWLEDGE LINKER ALGORITHM ============
@@ -435,14 +359,6 @@ def parseArguments():
         parser.add_argument('-p', type=int, required=False, default=4444, dest='port', help='Specify on which port shall be listened.')
         return parser.parse_args()
 
-def readData(dataset):
-    df = pd.read_table(dataset, sep=',', header=0)
-    log.info('Read data: {} {}'.format(df.shape, basename(dataset)))
-    spo_df = df.dropna(axis=0, subset=['sid', 'pid', 'oid'])
-    log.info('Note: Found non-NA records: {}'.format(spo_df.shape))
-    df = spo_df[['sid', 'pid', 'oid']].values
-    return df[:,0].astype(_int), df[:,1].astype(_int), df[:,2].astype(_int), spo_df
-
 def load_shape():
     with open(SHAPE, 'r') as shapeFile:
         line = shapeFile.readline()
@@ -451,92 +367,6 @@ def load_shape():
         line = line.replace(' ', '')
         split = line.split(',')
         return (int(split[0]), int(split[1]), int(split[2]))
-
-def ensureValidPaths(args):
-    outdir = abspath(expanduser(args.outdir))
-    assert exists(outdir)
-    args.outdir = outdir
-    datafile = abspath(expanduser(args.dataset))
-    assert exists(datafile)
-    args.dataset = datafile
-
-def executeBatch(args, G, spo_df, relsim, subs, preds, objs):
-	base = splitext(basename(args.dataset))[0]
-	t1 = time()
-	if args.method == 'stream': # KNOWLEDGE STREAM (KS)
-		# compute min. cost flow
-		log.info('Computing KS for {} triples..'.format(spo_df.shape[0]))
-		with warnings.catch_warnings():
-			warnings.simplefilter("ignore")
-			outjson = join(args.outdir, 'out_kstream_{}_{}.json'.format(base, DATE))
-			outcsv = join(args.outdir, 'out_kstream_{}_{}.csv'.format(base, DATE))
-			mincostflows, times = compute_mincostflow_legacy(G, relsim, subs, preds, objs, outjson)
-			# save the results
-			spo_df['score'] = mincostflows
-			spo_df['time'] = times
-			spo_df = normalize(spo_df)
-			spo_df.to_csv(outcsv, sep=',', header=True, index=False)
-			log.info('* Saved results: %s' % outcsv)
-		log.info('Mincostflow computation complete. Time taken: {:.2f} secs.\n'.format(time() - t1))
-	elif args.method == 'relklinker': # RELATIONAL KNOWLEDGE LINKER (KL-REL)
-		log.info('Computing KL-REL for {} triples..'.format(spo_df.shape[0]))
-		scores, paths, rpaths, times = compute_relklinker(G, relsim, subs, preds, objs)
-		# save the results
-		spo_df['score'] = scores
-		spo_df['path'] = paths
-		spo_df['rpath'] = rpaths
-		spo_df['time'] = times
-		spo_df = normalize(spo_df)
-		outcsv = join(args.outdir, 'out_relklinker_{}_{}.csv'.format(base, DATE))
-		spo_df.to_csv(outcsv, sep=',', header=True, index=False)
-		log.info('* Saved results: %s' % outcsv)
-		log.info('Relatioanal KL computation complete. Time taken: {:.2f} secs.\n'.format(time() - t1))
-	elif args.method == 'klinker':
-		log.info('Computing KL for {} triples..'.format(spo_df.shape[0]))
-		scores, paths, rpaths, times = compute_klinker(G, subs, preds, objs)
-		# save the results
-		spo_df['score'] = scores
-		spo_df['path'] = paths
-		spo_df['rpath'] = rpaths
-		spo_df['time'] = times
-		spo_df = normalize(spo_df)
-		outcsv = join(args.outdir, 'out_klinker_{}_{}.csv'.format(base, DATE))
-		spo_df.to_csv(outcsv, sep=',', header=True, index=False)
-		log.info('* Saved results: %s' % outcsv)
-		log.info('KL computation complete. Time taken: {:.2f} secs.\n'.format(time() - t1))
-	elif args.method == 'predpath': # PREDPATH
-		vec, model = predpath_train_model(G, spo_df) # train
-		print 'Time taken: {:.2f}s\n'.format(time() - t1)
-		# save model
-		predictor = { 'dictvectorizer': vec, 'model': model }
-		try:
-			outpkl = join(args.outdir, 'out_predpath_{}_{}.pkl'.format(base, DATE))
-			with open(outpkl, 'wb') as g:
-				pkl.dump(predictor, g, protocol=pkl.HIGHEST_PROTOCOL)
-			print 'Saved: {}'.format(outpkl)
-		except IOError, e:
-			raise e
-	elif args.method == 'pra': # PRA
-		features, model = pra_train_model(G, spo_df)
-		print 'Time taken: {:.2f}s\n'.format(time() - t1)
-		# save model
-		predictor = { 'features': features, 'model': model }
-		try:
-			outpkl = join(args.outdir, 'out_pra_{}_{}.pkl'.format(base, DATE))
-			with open(outpkl, 'wb') as g:
-				pkl.dump(predictor, g, protocol=pkl.HIGHEST_PROTOCOL)
-			print 'Saved: {}'.format(outpkl)
-		except IOError, e:
-			raise e
-	elif args.method in ('katz', 'pathent', 'simrank', 'adamic_adar', 'jaccard', 'degree_product'):
-		scores, times = link_prediction(G, subs, preds, objs, selected_measure=args.method)
-		# save the results
-		spo_df['score'] = scores
-		spo_df['time'] = times
-		spo_df = normalize(spo_df)
-		outcsv = join(args.outdir, 'out_{}_{}_{}.csv'.format(args.method, base, DATE))
-		spo_df.to_csv(outcsv, sep=',', header=True, index=False)
-		print '* Saved results: %s' % outcsv
 
 def execute(method, G, relsim, subId, predId, objId):
     """
@@ -563,19 +393,6 @@ def execute(method, G, relsim, subId, predId, objId):
     elif method in ('katz', 'pathent', 'simrank', 'adamic_adar', 'jaccard', 'degree_product'):
         scores, times = link_prediction(G, [subId], [predId], [objId], selected_measure=method)
         return scores[0]
-
-def batch(args, G, relsim):
-    # ensure input file and output directory is valid.
-    ensureValidPaths(args)
-    log.info('Launching {}..'.format(args.method))
-    log.info('Dataset: {}'.format(basename(args.dataset)))
-    log.info('Output dir: {}'.format(args.outdir))
-
-    # read data
-    subs, preds, objs, spo_df = readData(args.dataset)
-
-    # execute
-    executeBatch(args, G, spo_df, relsim, subs, preds, objs)
 
 def listen(connections=10, port=4444):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -651,10 +468,6 @@ def serviceClient(method, client, graph, relsim):
         except socket.error as ex:
             log.info('Socket error occured.')
             return
-        except rdflib.plugins.parsers.notation3.BadSyntax as ex:
-            log.info('Exception while parsing: "{}"'.format(request))
-            client.send("PARSING ERROR")
-            continue
         except KeyError as ex:
             client.send("ID ERROR")
             continue
